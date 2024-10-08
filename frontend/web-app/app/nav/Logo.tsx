b2a380a7de32c683bc5b1f45e82c28b594f64f30
aandoney <angel_andoney@live.com.mx>
2024-10-07 21:58:20 -0600

    Added NextAuth

--- frontend/web-app/app/nav/Logo.tsx
+++ frontend/web-app/app/nav/Logo.tsx
@@ -1,16 +1,25 @@
 'use client'
 
 import { useParamsStore } from '@/hooks/useParamsStore'
+import { usePathname, useRouter } from 'next/navigation'
 import React from 'react'
 import { AiOutlineCar } from 'react-icons/ai'
 
 export default function Logo() {
+    const router = useRouter();
+    const pathname = usePathname();
+
+    function doReset() {
+        if (pathname !== '/') router.push('/');
+        reset();
+    }
+
     const reset = useParamsStore(state => state.reset);
 
     return (
         <div
             className='flex items-center gap-2 text-3xl font-semibold text-red-500 cursor-pointer'
-            onClick={reset}>
+            onClick={doReset}>
             <AiOutlineCar size={34} />
             <div>Carsties Auctions</div>
         </div>
